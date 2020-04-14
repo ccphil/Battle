e7ba86569578446d9969f28e7c6f7ae8424fa456
Phil <ccpapps@outlook.com>
2020-04-14 20:36:15 +0200

    initial commit

---
+++ classes/game.py
@@ -0,0 +1,89 @@
+# we start by defining by classes.
+# We want colours in terminal
+import random
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp  # tells us what our MAX hitpoints are.
+        self.hp = hp  # tells us what CURRENT hitpoints are.
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10  # Attack low
+        self.atkh = atk + 10  # Attack high
+        self.df = df
+        self.magic = magic  # 'magic' object will be a dictionary of different magic spells with their associated mana costs.
+        self.action = ["Attack", "Magic"]  # Displaying what we can do every turn.
+
+    # Function to create random dmg, to make it more dynamaic.
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    # next are some utility classes defined.
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):  # this function takes 'cost' as a parameter.
+        self.mp -= cost  # dont need to return anything
+
+    def get_spell_name(self, i):  # pass it an index number
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        print("Actions")
+        for item in self.action:
+            print(str(i) + ":", item)  # pint index number to string.
+            i += 1  # going to increment i
+
+    def choose_magic(self):
+        i = 1
+        print("Magic")
+        for spell in self.magic:
+            print(
+                str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")"
+            )  # convert indexnr. to str and glue it together with the spell.
+            i += 1
+
+
+"""    def choose_magic(self):
+        i = 1
+
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "    MAGIC:" + bcolors.ENDC)
+        for spell in self.magic:
+            print(" " + str(i) + ":", spell.["name"], "(cost:", str(spell.cost) + ")")
+            i += 1"""
---
+++ main.py
@@ -0,0 +1,35 @@
+from classes.game import Person, bcolors
+
+# imported classes ('Person', 'bcolor') from  'game.py'.
+
+# Build a magic list filled with dictionaries.
+magic = [
+    {"name": "Fire", "cost": 10, "dmg": 60},
+    {"name": "Thunder", "cost": 10, "dmg": 80},
+    {"name": "Blizzard", "cost": 10, "dmg": 60},
+]
+
+# Next we instantiate the 'person' class named 'player' with hp, mp, atk, df, and magic spells.
+player = Person(460, 65, 60, 34, magic)
+
+# Next we instantaite the enemy. We are gonna reuse the 'Person' class that contains all the needed varaibles.
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+# Using the 'bcolors' class we provide formating and color to the text. In order to limit the effect we need to end the statement with 'bcolors.ENDC'
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
+
+
+# Be careful this doesnt run indefinitly and cause stack overflow.
+# Remember that in programming lists/arrays start at 0. in order to fix this we have to reduce our choice by 1 in oder to get spell nr 1.
+while running:
+    print("=========================================")
+    # player.choose_action()
+    player.choose_magic()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+
+    print("You chose", player.get_spell_name(int(index)))
+
+    running = False
